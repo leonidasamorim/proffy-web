244e55ab8d5ccff757eb7fed64861082635fdc85
Leonidas Amorim <leonidasamorim@gmail.com>
2020-08-04 09:26:35 -0300

    style header PageHeader

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
 
 interface PageHeaderProps {
-    title: string;
+  title: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
-    return(
-        <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                     <img src={backIcon} alt="Voltar" />
-                    </Link>
+  return (
+    <header className="page-header">
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar" />
+        </Link>
 
-                    <img src={logoImg} alt="Proffy" />
-                </div>
+        <img src={logoImg} alt="Proffy" />
+      </div>
 
+      <div className="header-content">
+        <strong>{props.title}</strong>
 
-            <div className="header-content">
-                <strong>{props.title}</strong>
-            </div>
-
-            </header>
-    )
-}
+        {props.children}
+      </div>
+    </header>
+  );
+};
 
 export default PageHeader;
