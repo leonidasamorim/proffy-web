95ddfb29ae998b7b15ac17e928c0b93c614e9248
Leonidas Amorim <leonidasamorim@gmail.com>
2020-08-08 17:23:14 -0300

    ajust css

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React from "react";
 
-import PageHeader from '../../components/PageHeader';
+import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+import "./styles.css";
 
-import './styles.css';
+import warningIcoin from "../../assets/images/icons/warning.svg";
 
-function TeacherForm () {
-    return(
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas." />
-        </div>
-    )
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulario de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcoin} alt="Aviso Importante" className="src" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
